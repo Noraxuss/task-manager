12d3ac91992ac6465ac9505c2c40596a7749ea15
Noraxus <vargaadam1993@gmail.com>
2026-01-22 22:05:26 +0100

    Add Vite and React setup

--- js/templates/color-change.js
+++
@@ -1,7 +0,0 @@
-export function changeColor(element, color) {
-    if (element && color) {
-        element.style.backgroundColor = color;
-    } else {
-        console.warn("Element or color not provided for color change.");
-    }
-}
